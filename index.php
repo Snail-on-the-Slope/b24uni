543c6156d063171cac5120f7faaca25a315b3c08
G <gudzenko.pr@gmail.com>
2020-05-10 01:13:28 +0300

    test

--- index.php
+++ index.php
@@ -3,6 +3,5 @@ echo "hello world"
 // $output = shell_exec("python myscript.py");
 // var_dump($output); 
 // echo "succes"; 
-is_callable('shell_exec') && false === stripos(ini_get('disable_functions'), 'shell_exec');
 echo "succes";
 ?>
