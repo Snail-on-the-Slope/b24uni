cb3100f44f014c59e1a7f85a050a2afc3d800af3
G <gudzenko.pr@gmail.com>
2020-05-10 01:06:07 +0300

    test

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ echo "hello world"
 // $output = shell_exec("python myscript.py");
 // var_dump($output); 
 // echo "succes"; 
-echo is_callable('shell_exec') && false === stripos(ini_get('disable_functions'), 
-'shell_exec');
+is_callable('shell_exec') && false === stripos(ini_get('disable_functions'), 'shell_exec');
+echo "succes";
 ?>
