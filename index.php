4bbabf3507791c566b0927d3431d3d576d533bbb
G <gudzenko.pr@gmail.com>
2020-05-10 11:59:48 +0300

    test

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<?php
-echo "hello world";
-$output = shell_exec("python myscript.py");
-var_dump($output);
-echo "succes";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pluging</title>
+</head>
+<body>
+    <h1>
+        <?php
+        echo "hello world";
+        $output = shell_exec("python myscript.py");
+        var_dump($output);
+        echo "succes";
+        ?>
+    </h1>
+</body>
+</html>
