0ac5db624ecde5935c53edecb06190266a39f592
G <gudzenko.pr@gmail.com>
2020-05-11 17:56:52 +0300

    test

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +9,7 @@
 </head>
 <body>
     <div class="parsing-google-sheets">
-        <form action="#"  method="post">
+        <form action=""  method="post">
             <label for="id-table">ID таблицы:</label>
             <input type="text" id="id-table" name="id-table" value="1Q5bw0D9y3_WfyhFhimr7_GgJZxxUNSMltOEW7WAFsuo">
             <label for="id-sheet">ID листа:</label>
@@ -20,12 +21,9 @@
         echo "hello world\n";
         ?>
         <?php
-            
-                $in = "sort"
+                $in = "sort";
                 $out = "python myscript.py " . $in;
                 echo $out;
-                $output = shell_exec($out);
-                echo $output;
         ?>
         </h1>
     </div>
