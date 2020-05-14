7bb9f2bdcd8a187c821e38e309d80d789a7473dc
G <gudzenko.pr@gmail.com>
2020-05-15 00:38:42 +0300

    test

--- import.py
+++ import.py
@@ -49,7 +49,7 @@ def get_urls(service, spreadsheetId, name, cells):
     
 
 def main(array):
-    print(array)
+    print(type(array), array, array.append(1))
 
 if __name__ == '__main__':
     main(sys.argv[1])
