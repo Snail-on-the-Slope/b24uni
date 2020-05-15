6b7952645e60be66d6208c373554e658731625ea
G <gudzenko.pr@gmail.com>
2020-05-15 16:18:08 +0300

    test

--- index.php
+++ index.php
@@ -1,75 +1,192 @@
-<?
-function redirect($url)
-{
-    Header("HTTP 302 Found");
-    Header("Location: ".$url);
-    die();
-}
-
-define('APP_ID', 'local.5e9c87d0066a39.52381824'); // take it from Bitrix24 after adding a new application
-define('APP_SECRET_CODE', 'l0nd2JaMBkWHNUJ40R8sj26AJt7pJ4MocRmUUMeWQl14RTZj2D'); // take it from Bitrix24 after adding a new application
-define('APP_REG_URL', 'https://b24uni.herokuapp.com/'); // the same URL you should set when adding a new application in Bitrix24
-
-
-$domain = isset($_REQUEST['portal']) ? $_REQUEST['portal'] : ( isset($_REQUEST['domain']) ? $_REQUEST['domain'] : 'empty');
-
-$step = 0;
-
-if (isset($_REQUEST['portal'])) $step = 1;
-if (isset($_REQUEST['code']))$step = 2;
-
-$btokenRefreshed = null;
-
-$arScope = array('user');
-
-switch ($step) {
-    case 1:
-        // we need to get the first authorization code from Bitrix24 where our application is _already_ installed
-        requestCode($domain);
-        break;
-
-    case 2:
-        //we've got the first authorization code and use it to get an access_token and a refresh_token (if you need it later)
-        echo "step 2 (getting an authorization code):<pre>";
-        print_r($_REQUEST);
-        echo "</pre><br/>";
-
-        $arAccessParams = requestAccessToken($_REQUEST['code'], $_REQUEST['server_domain']);
-        echo "step 3 (getting an access token):<pre>";
-        print_r($arAccessParams);
-        echo "</pre><br/>";
-
-        $arCurrentB24User = executeREST($arAccessParams['client_endpoint'], 'user.current', array(),
-            $arAccessParams['access_token']);
-
-        break;
-    default:
-        break;
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Quick start. Local server-side application without UI in Bitrix24</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <title>Pluging</title>
 </head>
 <body>
-<?if ($step == 0) {?>
-    step 1 (redirecting to Bitrix24):<br/>
-    <form action="" method="post">
-        <input type="text" name="portal" placeholder="Bitrix24 URL">
-        <input type="submit" value="Authorize">
-    </form>
-    <?
-}
-elseif ($step == 2) {
-    echo $arCurrentB24User["result"]["NAME"] . " " . $arCurrentB24User["result"]["LAST_NAME"];
-}
-?>
+    <div class="parsing-google-sheets">
+        <form action=""  method="post">
+            <label for="idTable">ID таблицы</label>
+            <input type="text" id="idTable" name="idTable" value="1Q5bw0D9y3_WfyhFhimr7_GgJZxxUNSMltOEW7WAFsuo">
+            <label for="idSheet">ID листа</label>
+            <input type="text" id="idSheet" name="idSheet" value="332621208">
+            <label for="cells">Ячейки URL</label>
+            <input type="text" id="cells" name="cells" value="A4:A181">
+            <input type="submit" name="SubmitParsing" value="Парсинг и категоризация данных в таблице">
+            <p>процесс займет некоторое время...</p>
+        </form>
+        <h1>  
+        <?php
+            if(isset($_POST['SubmitParsing'])){
+                $idTable = $_POST['idTable'];
+                $idSheet = $_POST['idSheet'];
+                $cells = $_POST['cells'];
+                if ($idTable != null && $idSheet != null && $cells != null) {
+                    // $out = "python parsing.py " . $idTable . " " . $idSheet . " " . $cells;
+                    // $output = shell_exec($out);
+                    $time  = date("H:i:s", mktime(date("H")+3, date("i")+3, date("s")+3, 0, 0, 0));
+                    echo "Парсинг завершен ", $time;
+                }
+            }
+        ?>
+        </h1>
+    </div>
+
+    <div class="import-data">
+        <form action="#" method="post">
+            <div class="obligatory">
+                <label for="idTableImport">ID таблицы</label>
+                <input type="text" id="idTableImport" name="idTableImport" value="1Q5bw0D9y3_WfyhFhimr7_GgJZxxUNSMltOEW7WAFsuo">
+                <label for="idSheetImport">ID листа</label>
+                <input type="text" id="idSheetImport" name="idSheetImport" value="332621208">
+            </div>
+            <p>Создание / Обновление карточки компании с полями и значениями:</p>
+            <div class="optional" id="add_field_area">
+                <div class="all-field">
+                    <div class="field-optional">
+                        <label for="fieldimport">Поле</label>
+                        <input type="text" id="fieldimport" name="fieldimport" value="URL">
+                    </div>
+                    <div class="cells-optional">
+                        <label for="cellsimport">Ячейки</label>
+                        <input type="text" id="cellsimport" name="cellsimport" value="A4:A181">
+                    </div>
+                    <button onclick="return addField();" class="plus-button">+</button>
+                    <button onclick="return deleteField(this);" class="plus-button hide">-</button>
+                </div>
+                <div class="error"></div>
+            </div>
+            <div id="new_fields"></div>
+            <input type="submit" name="SubmitImport" value="Импортировать данные из Google Sheets">
+        </form>
+        <textarea name="import-area" id="import-area" cols="30" rows="10" disabled>
+            <?php
+                if(isset($_POST['SubmitImport'])){
+                    $array = [];
+                    $k = 0;
+                    foreach($_POST as $value) {
+                        if ($k<(count($_POST)-1)) {
+                            $array[] =  $value;
+                            $k+=1;
+                        }
+                    }
+                    //$outImport = "python import.py " . escapeshellarg(json_encode($array));
+                    //$outputImport = shell_exec($outImport);
+                    //$data_table = json_decode($outputImport);
+                    //echo count($data_table[0])," компаний найдено. ";
+                    echo "||||- ";
+
+                    define('APP_ID', 'local.5ebe63d7585bb6.31756347'); // take it from Bitrix24 after adding a new application
+                    define('APP_SECRET_CODE', 'ievod89YV39EqGlJPqGYBbW6wC98Z0ZoBF4Ji3NZkiCEAz7NaO'); // take it from Bitrix24 after adding a new application
+                    define('APP_REG_URL', 'https://b24uni.herokuapp.com/'); // the same URL you should set when adding a new application in Bitrix24
+                    $_REQUEST['APP_ID'] = 'local.5ebe63d7585bb6.31756347';
+                    $_REQUEST['APP_SECRET_CODE'] = 'ievod89YV39EqGlJPqGYBbW6wC98Z0ZoBF4Ji3NZkiCEAz7NaO';
+                    $_REQUEST['APP_REG_URL'] = 'https://b24uni.herokuapp.com/';
+                    print_r($_REQUEST);
+                    echo ' - ';
+                    //requestCode($_REQUEST['DOMAIN']);
+                    print_r($_REQUEST["auth"])
+                    // $queryUrl = 'https://'.$_REQUEST['DOMAIN'].'/rest/user.current.json';
+                    // $queryData = http_build_query(array( "auth" => $_REQUEST['AUTH_ID'] ));
+
+                    // $curl = curl_init();
+                    // curl_setopt_array($curl, array(
+                    //     CURLOPT_SSL_VERIFYPEER => 0,
+                    //     CURLOPT_POST => 1,
+                    //     CURLOPT_HEADER => 0,
+                    //     CURLOPT_RETURNTRANSFER => 1,
+                    //     CURLOPT_URL => $queryUrl,
+                    //     CURLOPT_POSTFIELDS => $queryData,
+                    // ));
+
+                    // $result = json_decode(curl_exec($curl), true);
+                    // curl_close($curl);
+                }
+            ?>
+        </textarea>
+    </div>
+
+    <script>
+        var k = 0;
+        function addField() {
+            let elem = document.getElementById('add_field_area');
+            let newFields = document.getElementById('new_fields');
+
+            let clone = elem.cloneNode(true);
+
+            k++;
+            clone.id = clone.id + k;
+            clone.children[0].children[0].children[1].id = clone.children[0].children[0].children[1].id + k;
+            clone.children[0].children[0].children[1].name = clone.children[0].children[0].children[1].name + k;
+            clone.children[0].children[0].children[1].value = "";
+
+            clone.children[0].children[1].children[1].id = clone.children[0].children[1].children[1].id + k;
+            clone.children[0].children[1].children[1].name = clone.children[0].children[1].children[1].name + k;
+            clone.children[0].children[1].children[1].value = "";
+
+            clone.children[0].children[3].classList.remove('hide');
+
+            newFields.appendChild(clone);
+
+            let cellsimport = document.getElementById('cellsimport');
+            let error = clone.children[1];
+            clone.children[0].children[1].children[1].onblur = function() {
+                var numEl = cellsimport.value.match(/\d+/g),
+                numI = this.value.match(/\d+/g),
+                kol = 0,
+                flag=0;
+
+                if ( numEl.length == 2 ) {
+                    kol = parseInt(numEl[1]) - parseInt(numEl[0]);
+                    if (!((numI != null) && ( numI.length == 2 ) && (parseInt(numI[1]) - parseInt(numI[0]) == kol)) ) {
+                        flag++;
+                    }
+                } else {
+                    flag++;
+                }
+
+                if (flag!=0) {
+                    this.classList.add('invalid');
+                    error.innerHTML = 'Количество ячеек данного поля не совпадает с количеством ячеек поля URL'
+                }
+            };
+            clone.children[0].children[1].children[1].onfocus = function() {
+                if (this.classList.contains('invalid')) {
+                    this.classList.remove('invalid');
+                    error.innerHTML = "";
+                }
+            };
+
+            clone.children[0].children[0].children[1].onblur = function() {
+                if ((this.value=="")||(this.value==null)) {
+                    this.classList.add('invalid');
+                    error.innerHTML = 'Не заполнено название поля'
+                }
+            };
+            clone.children[0].children[0].children[1].onfocus = function() {
+                if (this.classList.contains('invalid')) {
+                    this.classList.remove('invalid');
+                    error.innerHTML = "";
+                }
+            };
+            return false;
+        }
+
+        function deleteField(a) {
+            var contDiv = a.parentNode.parentNode;
+            contDiv.parentNode.removeChild(contDiv);
+            k--;
+            return false;
+        }
+    </script>
 </body>
 </html>
-<?
+
+<?php
 function executeHTTPRequest ($queryUrl, array $params = array()) {
     $result = array();
     $queryData = http_build_query($params);
@@ -92,10 +209,16 @@ function executeHTTPRequest ($queryUrl, array $params = array()) {
     return $result;
 }
 
+function redirect($url) {
+    header("HTTP 302 Found");
+    header("Location: ".$url);
+    //die();
+}
+
 function requestCode ($domain) {
-    $url = 'https://' . $domain . '/oauth/authorize/' .
-        '?client_id=' . urlencode(APP_ID);
+    $url = 'https://' . $domain . '/oauth/authorize/' . '?client_id=' . urlencode(APP_ID);
     redirect($url);
+    print_r($_REQUEST);
 }
 
 function requestAccessToken ($code, $server_domain) {
@@ -111,3 +234,4 @@ function executeREST ($rest_url, $method, $params, $access_token) {
     $url = $rest_url.$method.'.json';
     return executeHTTPRequest($url, array_merge($params, array("auth" => $access_token)));
 }
+?>
