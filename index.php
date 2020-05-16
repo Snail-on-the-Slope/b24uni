6dfc962800e8f294a446a2e538e36e3624a5ddf9
G <gudzenko.pr@gmail.com>
2020-05-17 02:09:05 +0300

    test

--- index.php
+++ index.php
@@ -43,7 +43,8 @@
         <form action="#" method="post">
             <div class="obligatory">
                 <label for="idTableImport">ID таблицы</label>
-                <input type="text" id="idTableImport" name="idTableImport" value="1Q5bw0D9y3_WfyhFhimr7_GgJZxxUNSMltOEW7WAFsuo">
+                <input type="text" id="idTableImport" name="idTableImport"
+                    value="1Q5bw0D9y3_WfyhFhimr7_GgJZxxUNSMltOEW7WAFsuo">
                 <label for="idSheetImport">ID листа</label>
                 <input type="text" id="idSheetImport" name="idSheetImport" value="332621208">
             </div>
@@ -52,7 +53,16 @@
                 <div class="all-field">
                     <div class="field-optional">
                         <label for="fieldimport">Поле</label>
-                        <input type="text" id="fieldimport" name="fieldimport" value="URL">
+                        <select id="field_selection" name="field_selection">
+                            <option value="TITLE">Название компании</option>
+                            <option value="COMPANY_TYPE">Тип компании</option>
+                            <option value="WEB">Сайт</option>
+                            <option value="INDUSTRY">Сфера деятельности</option>
+                            <option value="REVENUE">Годовой оборот</option>
+                            <option value="EMPLOYESS">Кол-во сотрудников</option>
+                            <option value="COMMENTS">Комментарий</option>
+                            <option value="CUSTOM_FIELD">Пользовательское поле</option>
+                        </select>
                     </div>
                     <div class="cells-optional">
                         <label for="cellsimport">Ячейки</label>
@@ -60,6 +70,9 @@
                     </div>
                     <button onclick="return addField();" class="plus-button">+</button>
                     <button onclick="return deleteField(this);" class="plus-button hide">-</button>
+                </div>
+                <div id="add_name_custom_field" class="add_name_field hide">
+                    <input type="text" id="name_custom_field" name="name_custom_field" placeholder="Название поля">
                 </div>
                 <div class="error"></div>
             </div>
@@ -69,37 +82,44 @@
         <textarea name="import-area" id="import-area" cols="30" rows="10" disabled>
             <?php
                 if(isset($_POST['SubmitImport'])){
-                    $array = [];
-                    $name_fields = '';
-                    $k = 0;
-                    foreach($_POST as $value) {
-                        if ($k<(count($_POST)-1)) {
-                            $array[] =  $value;
-                            $k+=1;
-                            if ($k>2 && $k%2==1){
-                                $name_fields = $name_fields . $value . ' ';
-                            }
-                        }
-                    }
-                    $outImport = "python import.py " . escapeshellarg(json_encode($array));
-                    $outputImport = shell_exec($outImport);
-                    $data_table = json_decode($outputImport);
-                    echo "подключено к базе данных...  \n";
-                    if ($k==4) {
-                        echo count($data_table[0])," компаний найдено. ";
-                        $inport_data_table_to_js = '["' . implode('", "', $data_table[0]) . '"]';
-                    } else {
-                        $count_company = count($data_table);
-                        $count_item = count($data_table[0]);
-                        echo $count_company," компаний найдено. ";
-                        $inport_data_table_to_js = '';
-                        foreach ($data_table as $value) {
-                            foreach ($value as $item) {
-                                $inport_data_table_to_js = $inport_data_table_to_js . $item . ', ';
-                            }
-                        }
-                    }
-					$permission_to_connect_to_bitrix = 1;
+                    print_r($_POST);
+                    // $array = [];
+                    // $name_fields = '';
+                    // $k = 0;
+                    // $flag_k = false;
+                    // foreach($_POST as $value) {
+                    //     if ($k<(count($_POST)-1)) {
+                    //         if (k>1 && $k%2==0) {
+                    //             if ($value == 'CUSTOM_FIELD'){
+                    //                 $flag_k = true;
+                    //             }
+                    //         }
+                    //         $array[] =  $value;
+                    //         $k+=1;
+                    //         if ($k>2 && $k%2==1){
+                    //             $name_fields = $name_fields . $value . ' ';
+                    //         }
+                    //     }
+                    // }
+                    // $outImport = "python import.py " . escapeshellarg(json_encode($array));
+                    // $outputImport = shell_exec($outImport);
+                    // $data_table = json_decode($outputImport);
+                    // echo "подключено к базе данных...  \n";
+                    // if ($k==4) {
+                    //     echo count($data_table[0])," компаний найдено. ";
+                    //     $inport_data_table_to_js = '["' . implode('", "', $data_table[0]) . '"]';
+                    // } else {
+                    //     $count_company = count($data_table);
+                    //     $count_item = count($data_table[0]);
+                    //     echo $count_company," компаний найдено. ";
+                    //     $inport_data_table_to_js = '';
+                    //     foreach ($data_table as $value) {
+                    //         foreach ($value as $item) {
+                    //             $inport_data_table_to_js = $inport_data_table_to_js . $item . ', ';
+                    //         }
+                    //     }
+                    // }
+					// $permission_to_connect_to_bitrix = 1;
                 }
             ?>
         </textarea>
@@ -133,21 +153,33 @@
                     }
                 }
                 array.push(temp_temp);
-                alert(array);
-                alert(array[0]);
-                alert(array[0][0]);
-                alert(array[array.length-1]);
-                alert(array.length);
             }
 
             var name_fields = '<?php echo $name_fields;?>'.split(' ');
             name_fields = name_fields.slice(0, name_fields.length-1);
-            // alert(name_fields);
 
 			BX24.init(function(){
 				BX24.callMethod('user.current', {}, function(res){
 					textarea.innerHTML += '\n' + res.data().NAME + ' ' + res.data().LAST_NAME + '\n';
 				});
+
+                BX24.callMethod( "crm.company.add", 
+                    {
+                        fields:
+                        { 
+                            "TITLE": "ИП Титов",
+                            "COMPANY_TYPE": "CUSTOMER"	
+                        },
+                        params: { "REGISTER_SONET_EVENT": "Y" }		
+                    }, 
+                    function(result) 
+                    {
+                        if(result.error())
+                            console.error(result.error());
+                        else
+                            console.info("Создана компания с ID " + result.data());
+                    }
+                );
 			});
 
 			// BX24.callMethod(
@@ -164,32 +196,7 @@
 			// 	}
 			// );
 
-            // BX24.callMethod(
-            //     "crm.company.add", 
-            //     {
-            //         fields:
-            //         { 
-            //             "TITLE": "ИП Титов",
-            //             "COMPANY_TYPE": "CUSTOMER",
-            //             "INDUSTRY": "MANUFACTURING",
-            //             "EMPLOYEES": "EMPLOYEES_2",
-            //             "CURRENCY_ID": "RUB",
-            //             "REVENUE" : 3000000,
-            //             "LOGO": { "fileData": document.getElementById('logo') },
-            //             "OPENED": "Y", 
-            //             "ASSIGNED_BY_ID": 1, 
-            //             "PHONE": [ { "VALUE": "555888", "VALUE_TYPE": "WORK" } ] 	
-            //         },
-            //         params: { "REGISTER_SONET_EVENT": "Y" }		
-            //     }, 
-            //     function(result) 
-            //     {
-            //         if(result.error())
-            //             console.error(result.error());
-            //         else
-            //             console.info("Создана компания с ID " + result.data());
-            //     }
-            // );
+            
 
 			<?php $permission_to_connect_to_bitrix = 0;?>
 		}
@@ -208,57 +215,94 @@
             clone.children[0].children[0].children[1].id = clone.children[0].children[0].children[1].id + k;
             clone.children[0].children[0].children[1].name = clone.children[0].children[0].children[1].name + k;
             clone.children[0].children[0].children[1].value = "";
+            if (clone.children[0].children[0].children[1].classList.contains('invalid')) {
+                clone.children[0].children[0].children[1].classList.remove('invalid');
+            }
 
             clone.children[0].children[1].children[1].id = clone.children[0].children[1].children[1].id + k;
             clone.children[0].children[1].children[1].name = clone.children[0].children[1].children[1].name + k;
             clone.children[0].children[1].children[1].value = "";
+            if (clone.children[0].children[1].children[1].classList.contains('invalid')) {
+                clone.children[0].children[1].children[1].classList.remove('invalid');
+            }
 
             clone.children[0].children[3].classList.remove('hide');
 
+            clone.children[1].id = clone.children[1].id + k;
+            if (!clone.children[1].classList.contains('add_name_field')) {
+                clone.children[1].classList.add('add_name_field');
+            }
+            if (!clone.children[1].classList.contains('hide')) {
+                clone.children[1].classList.add('hide');
+            }
+            clone.children[1].children[0].id = clone.children[1].children[0].id + k;
+            clone.children[1].children[0].name = clone.children[1].children[0].name + k;
+
+            clone.children[2].innerHTML = '';
+
             newFields.appendChild(clone);
 
-            let cellsimport = document.getElementById('cellsimport');
-            let error = clone.children[1];
-            clone.children[0].children[1].children[1].onblur = function() {
-                var numEl = cellsimport.value.match(/\d+/g),
-                numI = this.value.match(/\d+/g),
-                kol = 0,
-                flag=0;
+            onblur_onfocus(clone.children[0].children[0].children[1], clone.children[1].children[0], clone.children[0].children[1].children[1], clone.children[2]);
+            var select_item = clone.children[0].children[0].children[1];
+            var div = clone.children[1];
+            select_item.addEventListener("click", function () { inputAddNameField(select_item, div) });
+            return false;
+        }
 
-                if ( numEl.length == 2 ) {
+        function onblur_onfocus(item1, item2, item3, error) {
+            let cellsimport = document.getElementById('cellsimport');
+            item3.onblur = function () {
+                var numEl = cellsimport.value.match(/\d+/g),
+                    numI = this.value.match(/\d+/g),
+                    kol = 0,
+                    flag = 0;
+
+                if (numEl!=null && numEl.length == 2) {
                     kol = parseInt(numEl[1]) - parseInt(numEl[0]);
-                    if (!((numI != null) && ( numI.length == 2 ) && (parseInt(numI[1]) - parseInt(numI[0]) == kol)) ) {
+                    if (!((numI != null) && (numI.length == 2) && (parseInt(numI[1]) - parseInt(numI[0]) == kol))) {
                         flag++;
                     }
                 } else {
                     flag++;
                 }
 
-                if (flag!=0) {
+                if (flag != 0) {
                     this.classList.add('invalid');
                     error.innerHTML = 'Количество ячеек данного поля не совпадает с количеством ячеек поля URL'
                 }
             };
-            clone.children[0].children[1].children[1].onfocus = function() {
+            item3.onfocus = function () {
                 if (this.classList.contains('invalid')) {
                     this.classList.remove('invalid');
                     error.innerHTML = "";
                 }
             };
 
-            clone.children[0].children[0].children[1].onblur = function() {
-                if ((this.value=="")||(this.value==null)) {
+            item2.onblur = function () {
+                if ((!this.classList.contains('hide')) && ((this.value == "") || (this.value == null))) {
                     this.classList.add('invalid');
                     error.innerHTML = 'Не заполнено название поля'
                 }
             };
-            clone.children[0].children[0].children[1].onfocus = function() {
+            item2.onfocus = function () {
                 if (this.classList.contains('invalid')) {
                     this.classList.remove('invalid');
                     error.innerHTML = "";
                 }
             };
-            return false;
+
+            item1.onblur = function () {
+                if (this.selectedIndex == -1) {
+                    this.classList.add('invalid');
+                    error.innerHTML = 'Не выбрано название поля'
+                }
+            };
+            item1.onfocus = function () {
+                if (this.classList.contains('invalid')) {
+                    this.classList.remove('invalid');
+                    error.innerHTML = "";
+                }
+            };
         }
 
         function deleteField(a) {
@@ -267,6 +311,65 @@
             k--;
             return false;
         }
+
+        function inputAddNameField(select, div) {
+            if (select.value == "CUSTOM_FIELD") {
+                if (div.classList.contains('hide')) {
+                    div.classList.remove('hide');
+                }
+            } else {
+                if (!div.classList.contains('hide')) {
+                    div.classList.add('hide');
+                }
+            }
+        }
+
+        var field = document.getElementById("add_field_area");
+        var select = document.getElementById("field_selection");
+        var div = document.getElementById("add_name_custom_field");
+
+        onblur_onfocus(select, div, document.getElementById("cellsimport"), field.querySelector('.error'));
+        select.addEventListener("click", function () { inputAddNameField(select, div) });
+
+        var error =  field.querySelector('.error');
+        document.getElementById("cellsimport").onblur = function () {
+            if ((this.value == "") || (this.value == null)) {
+                this.classList.add('invalid');
+                error.innerHTML = 'Не заполнено поле ячеек'
+            }
+        };
+        document.getElementById("cellsimport").onfocus = function () {
+            if (this.classList.contains('invalid')) {
+                this.classList.remove('invalid');
+                error.innerHTML = "";
+            }
+        };
+
+        div.onblur = function () {
+            if ((!this.classList.contains('hide')) && ((this.value == "") || (this.value == null))) {
+                this.classList.add('invalid');
+                error.innerHTML = 'Не заполнено название поля'
+            }
+        };
+        div.onfocus = function () {
+            if (this.classList.contains('invalid')) {
+                this.classList.remove('invalid');
+                error.innerHTML = "";
+            }
+        };
+
+        select.onblur = function () {
+            if (this.selectedIndex == -1) {
+                this.classList.add('invalid');
+                error.innerHTML = 'Не выбрано название поля'
+            }
+        };
+        select.onfocus = function () {
+            if (this.classList.contains('invalid')) {
+                this.classList.remove('invalid');
+                error.innerHTML = "";
+            }
+        };
     </script>
 </body>
 </html>
