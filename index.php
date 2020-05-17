3d70d366c1daed0d1160fd34012093e94f8c5dc9
G <gudzenko.pr@gmail.com>
2020-05-18 02:09:45 +0300

    test

--- index.php
+++ index.php
@@ -174,7 +174,8 @@
                             else {
                                 alert('22222222 ' + title_ + ' - ' + company_id + ' - ' + typeof  result.data() + ' - ' +  result.data() + ' : ' + result.data().length + ' - ' + result.data()['ID']);
                                 for (var i in result.data()){
-                                    alert(i + ' ---- ' + result.data()[i]);
+                                    for (j in result.data()[i])
+                                        alert(i + ' ---- ' + result.data()[i] + ' ||| ' + j + ' ---- ' + result.data()[i][j]);
                                 }
                                 company_id = result.data();
                             }
